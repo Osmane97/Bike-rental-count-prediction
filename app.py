a75e75761adc29a21479b7eafd78eae8329595f4
Osmane Boukara <oboukara97@gmail.com>
2026-04-03 07:50:37 +0100

    fix app api_URL

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import pandas as pd
 # ----------------------------
 # Config
 # ----------------------------
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "http://bike-app-alb-765107221.eu-west-2.elb.amazonaws.com/api/predict"
+
 
 st.title("🚲 Bike Rental Prediction")
 
